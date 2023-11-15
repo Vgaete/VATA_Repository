78201c09abed524b4c3768aea2324308b7bfde69
DrakonDV <rojascristofer569@gmail.com>
2023-11-14 23:10:21 -0300

    Reincorporacion a la base de datos, la habiamos malogrados durante la integracion de la solucion del boton, se crean nuevos modelos, se desconoce posibles implicaciones a futuro

--- VATA_djangoProject/VATA_mainApp/admin.py
+++ VATA_djangoProject/VATA_mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import testModel, Object, Place
+
 # Register your models here.
+admin.site.register(testModel)
+admin.site.register(Object)
+admin.site.register(Place)
--- VATA_djangoProject/VATA_mainApp/models.py
+++ VATA_djangoProject/VATA_mainApp/models.py
@@ -4,3 +4,15 @@ from django.db import models
 class testModel(models.Model):
     testText = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")  
+
+class Object(models.Model):
+    objectName = models.CharField(max_length=25)
+    objectCategory = models.CharField(max_length=6)
+    objectImage = models.ImageField(upload_to='objectImages/')
+    
+
+class Place(models.Model):
+    placeName = models.CharField(max_length=25)
+    location = models.CharField(max_length=500)
+    placeImage = models.ImageField(upload_to='placeImages/')
+    placeCategory = models.CharField(max_length=6)

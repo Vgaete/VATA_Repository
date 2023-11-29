2fb1c055ac93777480d0332d4b6678afce31fd6b
Vgaete <150606459+Vgaete@users.noreply.github.com>
2023-11-29 02:54:16 -0300

    sdads

--- VATA_djangoProject/VATA_djangoProject/settings.py
+++ VATA_djangoProject/VATA_djangoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4n*6lbg=lay&xpa1as&!+!j_z8j=&8ocyrsr+k3_jp%6cobcr@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ALLOWED_HOSTS = ['alococo4.pythonanywhere.com', '127.0.0.1']]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
